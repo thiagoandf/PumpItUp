d2d36684a14d4f770e44935785b89679050261a1
Caio Rulli <caiorulli@outlook.com>
2018-10-10 20:59:54 -0300

    Added main report script

---
+++ report.R
@@ -0,0 +1,13 @@
+# Pump it Up
+
+trainData <- read.csv("data/Training set values - 4910797b-ee55-40a7-8668-10efd5c1b960.csv", sep=",")
+trainData
+
+trainLabels <- read.csv("data/Training set labels - 0bf8bc6e-30d0-4c50-956a-603fc693d966.csv", sep=",")
+trainLabels
+
+testData <- read.csv("data/Test set values  - 702ddfc5-68cd-4d1d-a0de-f5f566f76d91.csv", sep=",")
+testData
+
+submissionFormat <- read.csv("data/SubmissionFormat.csv", sep=",")
+submissionFormat
