542b2c54af5414e8d1bd15e8eea0097bd28b11f7
Caio Rulli <caiorulli@outlook.com>
2018-10-10 21:35:36 -0300

    Initial pre-processing

--- report.R
+++ report.R
@@ -1,10 +1,16 @@
 # Pump it Up
 
 trainData <- read.csv("data/Training set values - 4910797b-ee55-40a7-8668-10efd5c1b960.csv", sep=",")
-trainData
+trainData <- trainData[order(trainData$id),]
 
 trainLabels <- read.csv("data/Training set labels - 0bf8bc6e-30d0-4c50-956a-603fc693d966.csv", sep=",")
-trainLabels
+trainLabels <- trainLabels[order(trainLabels$id),]
+
+status_group <- trainLabels$status_group
+
+trainDataset <- cbind(trainData, status_group)
+View(trainDataset)
+
 
 testData <- read.csv("data/Test set values  - 702ddfc5-68cd-4d1d-a0de-f5f566f76d91.csv", sep=",")
 testData
